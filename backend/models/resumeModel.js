646b7dd7066b9ef6b8542813d78ab50700bb66d1
Anees <aneesaboo123@gmail.com>
2025-09-13 09:17:59 +0530

    added:rresume

---
+++ backend/models/resumeModel.js
@@ -0,0 +1,85 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const resumeSchema = new Mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    thumbnailLink: {
+      type: String,
+    },
+    template: {
+      theme: String,
+      colorPalette: [String],
+    },
+    profileInfo: {
+      profilePreviewUrl: String,
+      fullName: String,
+      designation: String,
+      summary: String,
+    },
+
+    contactInfo: {
+      email: String,
+      phone: String,
+      location: String,
+      linkedin: String,
+      github: String,
+      website: String,
+    },
+
+    workExperience: {
+      company: String,
+      role: String,
+      startDate: String,
+      endDate: String,
+      description: String,
+    },
+
+    education: {
+      degree: String,
+      instituition: String,
+      startDate: String,
+      endDate: String,
+    },
+    skill: [
+      {
+        name: String,
+        progress: String,
+      },
+    ],
+    projects: [
+      {
+        title: String,
+        description: String,
+        github: String,
+        liveDemo: String,
+      },
+    ],
+
+    certification: [{ title: String, issuer: String, year: String }],
+
+    languages: [
+      {
+        name: String,
+        progress: Number,
+      },
+    ],
+
+    intrests: [String],
+  },
+  {
+    timeStamps: {
+      createdAt: "createAt",
+      updatedAt: "updateAt",
+    },
+  }
+);
+
+export default mongoose.model("Resume", resumeSchema);

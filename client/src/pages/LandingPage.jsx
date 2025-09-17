d20a792a3497120dbb488f97a187c7c459ebf899
Anees <aneesaboo123@gmail.com>
2025-09-18 01:01:27 +0530

    started:the landing page

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,9 +3,12 @@ import { landingPageStyles } from "../assets/dummystyle";
 import { Atom, MenuIcon, X } from "lucide-react";
 import { UserContext } from "../context/userContext";
 import { useNavigate } from "react-router-dom";
+import { ProfileInfoCard } from "../components/Cards";
 
 const LandingPage = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  // authentication
+  const [openAuthModel, setOpenAuthModel] = useState(false);
   // user data from usercontext
   const { user } = useContext(UserContext);
   const navigate = useNavigate();
@@ -35,9 +38,82 @@ const LandingPage = () => {
             )}
           </button>
           {/* nav links */}
-          <div className="hidden md:flex items-center">{user}</div>
+          <div className="hidden md:flex items-center">
+            {user ? (
+              <ProfileInfoCard />
+            ) : (
+              <button
+                className={landingPageStyles.desktopAuthButton}
+                onClick={() => setOpenAuthModel(true)}
+              >
+                <div
+                  className={landingPageStyles.desktopAuthButtonOverlay}
+                ></div>
+                <span className={landingPageStyles.desktopAuthButtonText}>
+                  Get Started
+                </span>
+              </button>
+            )}
+          </div>
         </div>
+        {/* mobile menu */}
+        {mobileMenuOpen && (
+          <div className={landingPageStyles.mobileMenu}>
+            <div className={landingPageStyles.mobileMenuContainer}>
+              {user ? (
+                <div className={landingPageStyles.mobileUserInfo}>
+                  <div className={landingPageStyles.mobileUserWelcome}>
+                    Welcome Back
+                  </div>
+                  <button
+                    onClick={() => {
+                      navigate("/dashboard");
+                      setMobileMenuOpen(false);
+                    }}
+                  >
+                    Go to Dashboard
+                  </button>
+                </div>
+              ) : (
+                <button
+                  className={landingPageStyles.mobileAuthButton}
+                  onClick={() => {
+                    setOpenAuthModel(true);
+                    setMobileMenuOpen(false);
+                  }}
+                >
+                  Get Started
+                </button>
+              )}
+            </div>
+          </div>
+        )}
       </header>
+      {/* main content */}
+      <main className={landingPageStyles.main}>
+        <section className={landingPageStyles.heroSection}>
+          <div className={landingPageStyles.heroGrid}>
+            {/* left Content */}
+            <div className={landingPageStyles.heroLeft}>
+              <div className={landingPageStyles.tagline}>
+                Professional Resume Builder
+              </div>
+              <h1 className={landingPageStyles.heading}>
+                <span className={landingPageStyles.headingText}>Craft</span>
+                <span className={landingPageStyles.headingGradient}>
+                  Professional
+                </span>
+                <span className={landingPageStyles.headingText}>Resumes</span>
+              </h1>
+              <p className={landingPageStyles.description}>
+                Create job-winning with expertly designed
+                templates.ATS-friendly,recruiter-approved,and tailored to your
+                career goal.
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };

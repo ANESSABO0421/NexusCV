220527aa2ab2d0699bd400470618db02ece85c03
Anees <aneesaboo123@gmail.com>
2025-10-07 11:40:51 +0530

    started:Login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Reac, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/userContext";
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+const Login = ({ setCurrentPage }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const { updateUser } = useContext(UserContext);
+  const navigate = useNavigate();
+  return <div>Login</div>;
+};
 
-export default Login
+export default Login;
